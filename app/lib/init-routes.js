4faaecc04cf78e6c7cb0b972cf575cbaa9b343fe
Rachel Self <rachel.m.self@gmail.com>
2014-05-07 22:11:42 -0500

    oceania functionality done

--- app/lib/init-routes.js
+++ app/lib/init-routes.js
@@ -14,8 +14,15 @@ module.exports = (req, res, next)=>{
 
 function load(app, fn){
   var home = traceur.require(__dirname + '/../routes/home.js');
+  var chimeras = traceur.require(__dirname + '/../routes/chimeras.js');
 
   app.get('/', home.index);
+  app.get('/about', home.about);
+  app.get('/chimeras', chimeras.index);
+  app.get('/chimeras/new', chimeras.new);
+  app.get('/chimeras/:id', chimeras.show);
+  app.post('/chimeras', chimeras.create);
+  app.post('/chimeras/:id/delete', chimeras.destroy);
   app.get('/help', home.help);
   console.log('Routes Loaded');
   fn();
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,7 +1,11 @@
 'use strict';
 
 exports.index = (req, res)=>{
-  res.render('home/index', {title: 'Node.js: Home'});
+  res.render('home/index', {title: 'Oceanic Aliens Home'});
+};
+
+exports.about = (req, res)=>{
+  res.render('home/about', {title: 'About Oceanic Aliens'});
 };
 
 exports.help = (req, res)=>{
